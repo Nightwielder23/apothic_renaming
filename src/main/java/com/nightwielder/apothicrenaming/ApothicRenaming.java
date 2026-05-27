a8a96093bcb1ca473478eb598aba0f1b2e182733
Nightwielder23 <nightwielder23@users.noreply.github.com>
2026-05-27 01:31:27 -0700

    removed config

--- src/main/java/com/nightwielder/apothicrenaming/ApothicRenaming.java
+++ src/main/java/com/nightwielder/apothicrenaming/ApothicRenaming.java
@@ -1,14 +1,11 @@
 package com.nightwielder.apothicrenaming;
 
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
 
 @Mod(ApothicRenaming.MODID)
 public class ApothicRenaming {
     public static final String MODID = "apothic_renaming";
 
     public ApothicRenaming() {
-        ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, Config.SPEC);
     }
 }
--- src/main/java/com/nightwielder/apothicrenaming/handler/AnvilRenameOverride.java
+++ src/main/java/com/nightwielder/apothicrenaming/handler/AnvilRenameOverride.java
@@ -1,7 +1,6 @@
 package com.nightwielder.apothicrenaming.handler;
 
 import com.nightwielder.apothicrenaming.ApothicRenaming;
-import com.nightwielder.apothicrenaming.Config;
 import com.nightwielder.apothicrenaming.util.ApotheosisDetector;
 import dev.shadowsoffire.apotheosis.adventure.affix.AffixHelper;
 import dev.shadowsoffire.apotheosis.adventure.loot.LootRarity;
@@ -19,7 +18,6 @@ public final class AnvilRenameOverride {
 
     @SubscribeEvent
     public static void onAnvilUpdate(AnvilUpdateEvent event) {
-        if (!Config.ENABLED.get()) return;
         if (!ApotheosisDetector.isApotheosisLoaded()) return;
         ItemStack left = event.getLeft();
         if (left.isEmpty()) return;
